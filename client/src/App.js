6bded7f277fd2ce3a6e93fda8e2510c2cf75d856
Jhenil Parihar <jhenilparihar@gmail.com>
2023-03-16 20:28:40 +0530

    integrated blockchain

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,140 @@
-import { useState } from 'react';
-import './App.css';
-import CreateAccount from './components/CreateAccount';
+import { useState, Component } from "react";
+import "./App.css";
+import Web3 from "web3";
+import axios from "axios";
 
-function App() {
-  const [status, setStatus] = useState('createAccount')
+import { contractABI, contractAddress } from "./utils/constants";
 
-  return (
-    <div className="text-blue-500">
-      <CreateAccount/>
-    </div>
-  );
+import ContractNotDeployed from "./components/ContractNotDeployed/ContractNotDeployed";
+import Loading from "./components/Loading/Loading";
+import CreateAccount from "./components/CreateAccount";
+import UploadImage from "./components/UploadImage/UploadImage";
+import Signup from "./components/Signup";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      accountAddress: "",
+      accountBalance: "",
+      count: 0,
+      loading: true,
+      metamaskConnected: false,
+      contractDetected: false,
+      Contract: null,
+      profileSet: false,
+      userProfile: null,
+    };
+  }
+
+  componentWillMount = async () => {
+    await this.loadWeb3();
+    await this.loadBlockchainData();
+  };
+
+  loadWeb3 = async () => {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert(
+        "Non-Ethereum browser detected. You should consider trying MetaMask!"
+      );
+    }
+  };
+
+  loadBlockchainData = async () => {
+    const web3 = window.web3;
+    const accounts = await web3.eth.getAccounts();
+    if (accounts.length === 0) {
+      this.setState({ metamaskConnected: false });
+    } else {
+      this.setState({ metamaskConnected: true });
+      this.setState({ loading: true });
+      this.setState({ accountAddress: accounts[0] });
+      let accountBalance = await web3.eth.getBalance(accounts[0]);
+      accountBalance = web3.utils.fromWei(accountBalance, "Ether");
+      this.setState({ accountBalance });
+      const Contract = new web3.eth.Contract(contractABI, contractAddress);
+      if (Contract) {
+        this.setState({ Contract });
+        this.setState({ contractDetected: true });
+      } else {
+        this.setState({ contractDetected: false });
+      }
+      const isProfileSet = await Contract.methods
+        .isProfileSet(this.state.accountAddress)
+        .call();
+      if (!isProfileSet) {
+        this.setState({ profileSet: false });
+      } else {
+        this.setState({ profileSet: true });
+        const cp = await this.state.Contract.methods
+          .getUserInfo(this.state.accountAddress)
+          .call();
+        console.log(cp);
+        this.setState({ userProfile: cp });
+      }
+      this.setState({ loading: false });
+    }
+  };
+
+  connectToMetamask = async () => {
+    await window.ethereum.enable();
+    this.setState({ metamaskConnected: true });
+    window.location.reload();
+  };
+
+  addToBlockchain = async (tokenPrice, message, keyword) => {
+    this.setState({ loading: true });
+    const price = window.web3.utils.toWei(tokenPrice.toString(), "ether");
+    const transactionHash = await this.state.Contract.methods
+      .addToBlockchain(this.state.accountAddress, price, message, keyword)
+      .send({ from: this.state.accountAddress })
+      .on("confirmation", () => {
+        localStorage.setItem(this.state.accountAddress, new Date().getTime());
+        this.setState({ loading: false });
+        window.location.reload();
+      });
+  };
+
+  createProfile = async (name) => {
+    this.setState({ loading: true });
+    console.log(this.state.Contract.methods);
+    const transactionHash = await this.state.Contract.methods
+      .addUserProfile(
+        name,
+        "https://ipfs.io/ipfs/QmTcn6vxMmxRvRGuCZvgjwxJ3NiBEiuDnQW64iwi4Bia5D"
+      )
+      .send({ from: this.state.accountAddress })
+      .on("confirmation", () => {
+        this.setState({ loading: false });
+        window.location.reload();
+      });
+  };
+
+  render() {
+    // console.log(this.state.userProfile?.tokenURI);
+    return (
+      <>
+        {!this.state.metamaskConnected || !this.state.profileSet ? (
+          <CreateAccount
+            connectToMetamask={this.connectToMetamask}
+            createProfile={this.createProfile}
+            metamaskConnected={this.state.metamaskConnected}
+            accountAddress={this.state.accountAddress}
+          />
+        ) : !this.state.contractDetected ? (
+          <ContractNotDeployed />
+        ) : this.state.loading ? (
+          <Loading />
+        ) : (
+          <Signup tokenURI={this.state.userProfile?.tokenURI} />
+        )}
+      </>
+    );
+  }
 }
 
 export default App;
--- client/src/components/CreateAccount.jsx
+++ client/src/components/CreateAccount.jsx
@@ -1,73 +1,86 @@
-import { createTheme, TextField, ThemeProvider } from "@mui/material"
+import { createTheme, TextField, ThemeProvider } from "@mui/material";
 import { useState } from "react";
 
-const CreateAccount = () => {
-    const [firstName, setFirstName] = useState('')
-    const [lastName, setLastName] = useState('')
+const CreateAccount = ({
+  connectToMetamask,
+  createProfile,
+  metamaskConnected,
+  accountAddress,
+}) => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
 
-    const darkTheme = createTheme({
-        palette: {
-          mode: 'dark',
-        },
-      });
+  const darkTheme = createTheme({
+    palette: {
+      mode: "dark",
+    },
+  });
 
-    const onConnectClickHandler = () => {
-        
-    }
+  const onCreateAccountHandler = (e) => {
+    e.preventDefault();
+    const name = e.target.firstName.value + " " + e.target.lastName.value;
+    createProfile(name);
+  };
 
-    const onCreateAccountHandler = (e) => {
-        e.preventDefault()
-        const name = e.target.firstName.value + e.target.lastName.value
-    }
+  return (
+    <div className="w-[500px] h-auto p-[20px] bg-[#12131A] text-white flex flex-col gap-y-[20px] rounded-lg m-auto mt-20">
+      <div className="flex flex-row">
+        {metamaskConnected ? (
+          <div className="text-center overflow-ellipsis w-[100px] h-[35px] overflow-hidden">
+            {accountAddress.substr(0, 5) +
+              "..." +
+              accountAddress.slice(accountAddress.length - 5)}
+          </div>
+        ) : (
+          <button
+            onClick={connectToMetamask}
+            className="bg-green-500 px-[40px] py-[5px] rounded-xl ml-auto font-semibold"
+          >
+            Connect
+          </button>
+        )}
+      </div>
+      <div className="text-[48px] font-semibold justify-center text-center">
+        Create Account
+      </div>
 
-    return (
-        <div className="w-[500px] h-auto p-[20px] bg-[#12131A] text-white flex flex-col gap-y-[20px] rounded-lg m-auto mt-20">
-            <div className="flex flex-row">
-                <button className="bg-green-500 px-[40px] py-[5px] rounded-xl ml-auto font-semibold">
-                    Connect
-                </button>
+      <form onSubmit={onCreateAccountHandler}>
+        <div className="flex flex-row gap-x-[20px]">
+          <ThemeProvider theme={darkTheme}>
+            <TextField
+              required
+              id="outlined-required"
+              name="firstName"
+              label="First Name"
+              placeholder="John"
+              size="small"
+            />
 
-                <div className="text-center overflow-ellipsis w-[100px] h-[35px] overflow-hidden">
-                    dkfsldkfjlskdfjlksdlfksjdlkfjs
-                </div>
-            </div>
-            <div className="text-[48px] font-semibold justify-center text-center">
-                Create Account
-            </div>
-
-            <form onSubmit={onCreateAccountHandler}>
-            <div className="flex flex-row gap-x-[20px]">
-            <ThemeProvider theme={darkTheme}>
-                <TextField
-                    required
-                    id="outlined-required"
-                    name="firstName"
-                    label="First Name"
-                    placeholder="John"
-                    size="small"
-                />
-
-                <TextField
-                    required
-                    id="outlined-required"
-                    name="lastName"
-                    label="Last Name"
-                    placeholder="John"
-                    size="small"
-                />
-            </ThemeProvider>
-            </div>
-
-            <button className="min-w-[200px] m-auto bg-primaryBlue py-[8px] px-[24px] rounded-lg text-white font-inter font-medium self-start border-[1px] mt-[40px]"
-                // onClick={() => {setShowConfirmPass(true)}}
-                type="submit"
-            >Create</button>
-            </form>
-
-        <div className="text-center text-[14px] mt-[20px]">Powered by VisualVault</div>
+            <TextField
+              required
+              id="outlined-required"
+              name="lastName"
+              label="Last Name"
+              placeholder="John"
+              size="small"
+            />
+          </ThemeProvider>
         </div>
 
-    )
-}
+        <button
+          className="min-w-[200px] m-auto bg-primaryBlue py-[8px] px-[24px] rounded-lg text-white font-inter font-medium self-start border-[1px] mt-[40px]"
+          // onClick={() => {setShowConfirmPass(true)}}
+          type="submit"
+        >
+          Create
+        </button>
+      </form>
 
-export default CreateAccount
+      <div className="text-center text-[14px] mt-[20px]">
+        Powered by VisualVault
+      </div>
+    </div>
+  );
+};
+
+export default CreateAccount;
