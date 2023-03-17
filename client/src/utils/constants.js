4f7bf86efc5e4ef846590a81b36d169da4ddd284
Jhenil Parihar <jhenilparihar@gmail.com>
2023-03-17 08:45:17 +0530

    contract reset

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from './VisualVault.json';
 
 export const contractABI = abi.abi;
-export const contractAddress = '0x1038B1ffc19C62D20D7F308A913605C58dfFDcF9';
+export const contractAddress = '0x95B42641D551664Aa74dB496a0Cd56cb97B54c37';
