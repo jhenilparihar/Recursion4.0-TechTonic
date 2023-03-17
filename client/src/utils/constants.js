58d27bc689cfa89e8dd047f1618305540c7dea9f
Jhenil Parihar <jhenilparihar@gmail.com>
2023-03-17 08:16:30 +0530

    commit

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from './VisualVault.json';
 
 export const contractABI = abi.abi;
-export const contractAddress = '0x53692b9B65D70Ec1D218980D4694175E8b73ecdd';
+export const contractAddress = '0x1038B1ffc19C62D20D7F308A913605C58dfFDcF9';
